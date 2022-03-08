a3f4631b6031289c6e108ced7ad0f574fcf7d770
huy2012 <huynn@reactplus.jp>
2022-03-08 23:10:01 +0700

    UP File from 09 / 09 / 2021 --->  17 : 56

--- python/de_quy.py
+++ python/de_quy.py
@@ -1,4 +1,7 @@
 '''
+link tham khảo
+https://quantrimang.com/ham-de-quy-trong-python-145807#:~:text=V%C3%AD%20d%E1%BB%A5%20v%E1%BB%81%20h%C3%A0m%20%C4%91%E1%BB%87%20quy%20trong%20Python&text=l%C3%A0%201*2*3*4*5%3D%20120.&text=Ph%C3%A9p%20%C4%91%E1%BB%87%20quy%20tr%C3%AAn%20k%E1%BA%BFt,t%E1%BB%B1%20g%E1%BB%8Di%20m%C3%A3i%20%C4%91%E1%BA%BFn%20n%C3%B3.
+
 Loop -> CPU
 Đệ quy -> RAM
 
