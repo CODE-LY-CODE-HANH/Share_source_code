216e6c4e6f8c872cf6e471ad14f0f8d40793f2a5
learn-game0905 <nqh149734@gmail.com>
2022-09-03 09:23:23 +0700

    UP File from 18 / 08 / 2022 --->  21 : 25

---
+++ python/python_co_ban/pham_vi_bien.py
@@ -0,0 +1,50 @@
+'''
+Biến toàn cục (global), biến cục bộ (local), biến nonlocal
+'''
+
+x1 = "Biến toàn cục" #khai báo biến x
+
+#Gọi x từ trong hàm vidu()
+def vidu():
+    global x1
+    x1 = x1 + " 4"
+    print("x trong hàm vidu() :", x1)
+
+
+vidu()
+
+#Gọi x ngoài hàm vidu()
+print("x ngoài hàm vidu() : ", x1)
+
+# def vidu():
+#    y = "Biến cục bộ"
+#    return y
+#
+# vidu()
+#
+# print(vidu())
+
+# x = 5
+#
+# def vidu():
+#    # global x
+#    x = 10
+#    print("Biến x cục bộ:", x)
+#
+# vidu()
+#
+# print("Biến x toàn cục:", x)
+
+def ham_ngoai():
+   x = "Biến cục bộ"
+
+   def ham_trong():
+      nonlocal x
+      x = "Biến nonlocal"
+      print("Bên trong:", x)
+
+   ham_trong()
+   print("Bên ngoài:", x)
+
+
+ham_ngoai()
